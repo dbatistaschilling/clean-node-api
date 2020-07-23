5aa7338b54cd6a6b394d85e7bca8b42f38f2f09a
davi schilling <davi.bschilling@mobilesoft.it>
2020-07-23 19:15:37 +0200

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../protocols/encrypter'
+export * from '../../protocols/add-account-repositoy'
 export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/add-account'
