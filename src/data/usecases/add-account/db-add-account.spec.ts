14175d5bdfed980369e4fd95c34f0c54161d2636
davi schilling <davi.bschilling@mobilesoft.it>
2020-07-23 18:43:12 +0200

    test: ensure DbAddAccount throw if encrypter throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,5 +1,6 @@
 import { Encrypter } from './../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
+import { rejects } from 'assert'
 
 interface SutTypes {
   sut: DbAddAccount
@@ -35,5 +36,17 @@ describe('DbAddAccount Usecase', () => {
     }
     await sut.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
+  })
+
+  test('should throws if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
   })
 })
